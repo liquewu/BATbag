59e1d372b2766b6ae700e2474497f2a07b6b8f0c
liquewu <liquewu@gmail.com>
2020-10-23 14:40:58 +0800

    Delete 截取视频.bat

--- 用例/截取视频.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 使用ffmpeg截取视频
-set FFMPEG_PATH="D:\BATbench\lib\ffmpeg.exe"
-
-REM 例子是从00:23:00开始截取90秒，在0:22:30附近查找到关键帧。然后在接下来的30秒内逐帧查找
-%FFMPEG_PATH% -ss 00:22:30 -i "原视频名称.mp4" -ss 00:00:30 -t 90 -y -c copy "原视频名称_output.mp4"
-
-REM pause
